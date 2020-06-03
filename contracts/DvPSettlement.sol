ca35f542ed03263b2f558438d49de3e6829c3d20
Melvillian <melvillevt@gmail.com>
2020-06-03 15:33:06 -0400

    fix compiler errors

--- contracts/DvPSettlement.sol
+++ contracts/DvPSettlement.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.16;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 
 /** @title Tokensoft Delivery vs. Payment Contract. Allows Buyer and Seller ERC20 tokens to be atomically
   * swapped when OK'ed by a Broker Dealer. This atomic swapping is called Delivery vs. Payment in traditional
@@ -14,7 +14,7 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Deta
   *
   * Assumes sellers and buyers have approved this contract to access their balances.
   */
-contract Exchange {
+contract DvPSettlement {
     // The broker dealer's address. This is the only key which can call the `settle()` method
     address public broker_dealer;
 
@@ -24,53 +24,53 @@ contract Exchange {
     mapping (uint128 => bool) public fills;
 
     // Fired after a call to `settle()`
-    event Settled(address indexed seller_address, uint seller_amount, address indexed seller_token,
-        address indexed buyer_address, uint buyer_amount, address indexed buyer_token, uint128 nonce);
+    event Settled(address seller_address, uint seller_amount, address indexed seller_token,
+        address buyer_address, uint buyer_amount, address indexed buyer_token, uint128 nonce);
 
     // Fired after any expected failure code path
     /**
-     * 1 -> “The buyer_address and seller_address must be different”
+     * 1 -> “The seller_address and buyer_address must be different”
      * 2 -> “This order has already been filled”
      * 3 -> “The call to settle() must be made by the broker_dealer”
      * 4 -> "No ether is required, this contract always settles two ERC20 tokens"
      */
-    Failed(uint code, address indexed seller_address, uint seller_amount, address indexed seller_token,
-        address indexed buyer_address, uint buyer_amount, address indexed buyer_token, uint128 nonce);
+    event Failed(uint code, address seller_address, uint seller_amount, address indexed seller_token,
+        address buyer_address, uint buyer_amount, address indexed buyer_token, uint128 nonce);
 
     function settle(address seller_address, uint seller_amount, address seller_token, address buyer_address,
         uint buyer_amount, address buyer_token, uint128 nonce, uint8 buyer_v, bytes32 buyer_r, bytes32 buyer_s,
-        uint8 seller_v, bytes32 seller_r, bytes32 seller_s) payable {
+        uint8 seller_v, bytes32 seller_r, bytes32 seller_s) external payable {
 
         // no making a trade with yourself
-        if (seller_address != taker_address) {
-            Failed(1, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
+        if (seller_address == buyer_address) {
+            emit_failed(1, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
             return;
         }
 
         // no reentrancy or replay attacks, make sure the nonce hasn’t been used yet
         if (fills[nonce] != true) {
-            Failed(2, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
+            emit_failed(2, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
             return;
         }
 
         // only the broker dealer can settle a Trade
         if (msg.sender != broker_dealer) {
-            Failed(3, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
+            emit_failed(3, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
             return;
         }
 
         if (msg.value != 0) {
-            Failed(4, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
+            emit_failed(4, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
             return;
         }
 
         // First verify that the seller has agreed to this Trade by verifying their signature against the Trade data
-        bytes32 hash = validate(seller_address, seller_address, seller_amount, seller_token,
+        bytes32 hash = verify(seller_address, seller_address, seller_amount, seller_token,
             buyer_address, buyer_amount, buyer_token,
             nonce, buyer_v, buyer_r, buyer_s);
 
         // Now do the same but for the buyer
-        bytes32 hash = validate(buyer_address, seller_address, seller_amount, seller_token,
+        hash = verify(buyer_address, seller_address, seller_amount, seller_token,
             buyer_address, buyer_amount, buyer_token,
             nonce, seller_v, seller_r, seller_s);
 
@@ -84,25 +84,23 @@ contract Exchange {
             buyer_address, buyer_amount, buyer_token));
 
         // Log an event to indicate completion.
-        Settled(seller_address, seller_amount, seller_token,
-            buyer_address, buyer_amount, buyer_token,
-            nonce);
+        emit_settled(seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
     }
 
-    /** Validates that either the buyer or seller have agreed to this Trade*/
+    /** Verifies that either the buyer or seller have agreed to this Trade*/
     function verify(address address_to_verify, address seller_address, uint seller_amount, address seller_token,
-                      address buyer_address, uint buyer_address, address buyer_token,
-                      uint256 nonce, uint8 v, bytes32 r, bytes32 s) private returns (bytes32) {
+                      address buyer_address, uint buyer_amount, address buyer_token,
+                      uint256 nonce, uint8 v, bytes32 r, bytes32 s) private pure returns (bytes32) {
 
         // Hash arguments to identify the order.
-        bytes32 hashV = keccak256(seller_address, seller_amount, seller_token,
+        bytes32 hashV = keccak256(abi.encode(seller_address, seller_amount, seller_token,
             buyer_address, buyer_amount, buyer_token,
-            nonce);
+            nonce));
 
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-        bytes32 prefixedHash = sha3(prefix, hashV);
+        bytes32 prefixedHash = keccak256(abi.encode(prefix, hashV));
 
-        require(ecrecover(prefixedHash, v, r, s) == address_to_verify);
+        require(ecrecover(prefixedHash, v, r, s) == address_to_verify, "signature verification failed");
 
         return hashV;
     }
@@ -123,7 +121,33 @@ contract Exchange {
       * returns true if, the transfer went through
       */
     function transfer(address from, address to, uint amount, address token) private returns (bool) {
-        require(ERC20(token).transferFrom(from, to, amount));
+        require(ERC20(token).transferFrom(from, to, amount), "transfer failed");
         return true;
+    }
+
+    /**
+     * Fire a Failed event with the appropriate failure code.
+     *
+     * We use this simple function to get around the unfortunate fact that if we called it inline, the compiler
+     * would complain about a "Stack Too Deep Error". See the following blog post for details:
+     * https://blog.aventus.io/stack-too-deep-error-in-solidity-5b8861891bae
+     */
+    function emit_failed(uint8 code, address seller_address, uint seller_amount, address seller_token, address buyer_address,
+        uint buyer_amount, address buyer_token, uint128 nonce) private {
+        emit Failed(code, seller_address, seller_amount, seller_token, buyer_address, buyer_amount, buyer_token, nonce);
+    }
+
+    /**
+     * Fire a Settled event
+     *
+     * We use this simple function to get around the unfortunate fact that if we called it inline, the compiler
+     * would complain about a "Stack Too Deep Error". See the following blog post for details:
+     * https://blog.aventus.io/stack-too-deep-error-in-solidity-5b8861891bae
+     */
+    function emit_settled(address seller_address, uint seller_amount, address seller_token,
+        address buyer_address, uint buyer_amount, address buyer_token, uint128 nonce) private {
+        emit Settled(seller_address, seller_amount, seller_token,
+            buyer_address, buyer_amount, buyer_token,
+            nonce);
     }
 }
